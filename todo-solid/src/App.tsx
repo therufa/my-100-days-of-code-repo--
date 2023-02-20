0d249a74bc7bc51db6b8b494354172850229d56f
Attila Max Ruf <therufa@gmail.com>
2023-02-20 21:53:22 +0100

    add basics

--- todo-solid/src/App.tsx
+++ todo-solid/src/App.tsx
@@ -1,10 +1,28 @@
 import { createSignal, type Component } from 'solid-js';
 
 const App: Component = () => {
-  const [todo, setTodo] = createSignal([]);
+  const [todo, setTodo] = createSignal<string[]>([]);
+  const [text, setText] = createSignal('');
 
   return (
-    <div>
+    <div class='text-red-700'>
+      <h1 class='text-2xl'>Todo List</h1>
+      <ul>
+        {todo().map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+      <input
+        type='text'
+        onInput={(e) => setText(e.currentTarget.value)}
+        value={text()}
+      />
+      <button
+        onClick={() => {
+          setTodo([...todo(), text()]);
+          setText('');
+        }}
+      >+</button>
     </div>
   );
 };
