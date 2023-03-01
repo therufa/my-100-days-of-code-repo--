6b63d9179fe8fa9581ad84a44e7c565f4658e589
Attila Max Ruf <therufa@gmail.com>
2023-03-01 11:31:54 +0100

    penith

--- ts-learning/types.ts
+++ ts-learning/types.ts
@@ -17,18 +17,33 @@ type SomeRecord = Partial<Record<SomeKeys, number>>;
 const someRecord: SomeRecord[] = [{ A: 1, B: 2 }, { B: 4 }];
 
 Object.entries(someRecord).forEach(([key, value]) => {
-  // value is of type SomeRecord
   // key is of type string
+  // value is of type SomeRecord
 });
 
-const objectEntries = <T extends {}>(obj: T): [keyof T, T[keyof T]][] =>
+const objectEntries = <T extends {}>(obj: T) =>
   Object.entries(obj) as [keyof T, T[keyof T]][];
 
-const a = objectEntries(compoundObj);
+const b = objectEntries(compoundObj);
+//    ^?
+
+const objEntries = Object.entries as unknown as <T extends {}>(
+  obj: T
+) => [keyof T, T[keyof T][]];
+const a = objEntries(compoundObj);
+//    ^?
+
+a;
+b;
+
 a.forEach(([key, value]) => {
-  // value is of type CompundType
-  // key is of type "pos" | "neg" | "posneg"
+  // key is of type SomeKeys
+  // value is of type number
   console.log(key, value);
 });
 
-a;
+b.forEach(([key, value]) => {
+  // key is of type SomeKeys
+  // value is of type number
+  console.log(key, value);
+});
