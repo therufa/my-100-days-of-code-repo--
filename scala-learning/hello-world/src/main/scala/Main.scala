ce1d34e6e32ebcf89bab16c1548ae30678c18f92
Attila Max Ruf <therufa@gmail.com>
2023-03-04 21:42:52 +0100

    experiment with cases and classes and guards. amen!

--- scala-learning/hello-world/src/main/scala/Main.scala
+++ scala-learning/hello-world/src/main/scala/Main.scala
@@ -1,4 +1,9 @@
 @main def hello: Unit =
+  println(area(circle))
+  println(area(rectangle))
+end hello
+
+def hellos: Unit =
   Sajt.increaseSajt()
   Sajt.increaseSajt()
   Sajt.increaseSajt()
@@ -7,9 +12,23 @@
   Sajt.decreaseSajt()
 
   println(s"Number of sajt ${Sajt.getSajt()}")
+end hellos
 
 object Sajt:
   var sajt = 1
   def increaseSajt(): Unit = sajt += 1
   def decreaseSajt(): Unit = sajt -= 1
   def getSajt(): Int = sajt
+end Sajt
+
+sealed trait Shape
+case class Circle(radius: Double) extends Shape
+case class Rectangle(width: Double, height: Double) extends Shape
+
+def area(shape: Shape): Double = shape match {
+  case Circle(r)       => math.Pi * r * r
+  case Rectangle(w, h) => w * h
+}
+
+val circle = Circle(5.0)
+val rectangle = Rectangle(3.0, 4.0)
